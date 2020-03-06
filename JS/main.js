db85e17f377ac1b0a17701c75f64659d3bec42e0
Kevin Dolan <c16377571@mytudublin.ie>
2020-03-06 10:50:58 +0000

    Added more html pages (home, about, etc), also added javascript code to record audio and visualise it using web audio API

--- JS/main.js
+++ JS/main.js
@@ -2,51 +2,160 @@
 //import ('https://p5js.org/assets/js/p5.dom.min.js')
 //import ('https://p5js.org/assets/js/p5.sound.min.js')
 
-let mic;
+function init() {
+  //heading.textContent = 'Voice-change-O-matic';
 
-function setup(){
-  let cnv = createCanvas(100, 100);
-  cnv.mousePressed(userStartAudio);
-  textAlign(CENTER);
-  mic = new p5.AudioIn();
-  mic.start();
-}
+  // Older browsers might not implement mediaDevices at all, so we set an empty object first
+  if (navigator.mediaDevices === undefined) {
+    navigator.mediaDevices = {};
+  }
 
-function draw(){
-  background(0);
-  fill(255);
-  text('tap to start', width/2, 20);
 
-  micLevel = mic.getLevel();
-  let y = height - micLevel * height;
-  ellipse(width/2, y, 10, 10);
+  // Some browsers partially implement mediaDevices. We can't just assign an object
+  // with getUserMedia as it would overwrite existing properties.
+  // Here, we will just add the getUserMedia property if it's missing.
+  if (navigator.mediaDevices.getUserMedia === undefined) {
+    navigator.mediaDevices.getUserMedia = function(constraints) {
+
+      // First get ahold of the legacy getUserMedia, if present
+      var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+      // Some browsers just don't implement it - return a rejected promise with an error
+      // to keep a consistent interface
+      if (!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+      }
+
+      // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+      return new Promise(function(resolve, reject) {
+        getUserMedia.call(navigator, constraints, resolve, reject);
+      });
+    }
+  }
+
+  // set up forked web audio context, for multiple browsers
+    // window. is needed otherwise Safari explodes
+
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    var voiceSelect = document.getElementById("record");
+    var source;
+    var stream;
+
+    //set up the different audio nodes we will use for the app
+
+    var analyser = audioCtx.createAnalyser();
+    analyser.minDecibels = -90;
+    analyser.maxDecibels = -10;
+    analyser.smoothingTimeConstant = 0.85;
+
+    var distortion = audioCtx.createWaveShaper();
+    var gainNode = audioCtx.createGain();
+    var biquadFilter = audioCtx.createBiquadFilter();
+    var convolver = audioCtx.createConvolver();
+
+    // set up canvas context for visualizer
+
+  var canvas = document.querySelector('.visualizer');
+  var canvasCtx = canvas.getContext("2d");
+
+  var intendedWidth = document.querySelector('.wrapper').clientWidth;
+
+  canvas.setAttribute('width',intendedWidth);
+
+  var visualSelect = document.getElementById("record");
+
+  var drawVisual;
+
+  //main block for doing the audio recording
+
+  if (navigator.mediaDevices.getUserMedia) {
+     console.log('getUserMedia supported.');
+     var constraints = {audio: true}
+     navigator.mediaDevices.getUserMedia (constraints)
+        .then(
+          function(stream) {
+             source = audioCtx.createMediaStreamSource(stream);
+             source.connect(distortion);
+             distortion.connect(biquadFilter);
+             biquadFilter.connect(gainNode);
+             convolver.connect(gainNode);
+             gainNode.connect(analyser);
+             analyser.connect(audioCtx.destination);
+
+          	 visualize();
+        })
+        .catch( function(err) { console.log('The following gUM error occured: ' + err);})
+  } else {
+     console.log('getUserMedia not supported on your browser!');
+  }
+
+  function visualize() {
+    WIDTH = canvas.width;
+    HEIGHT = canvas.height;
+
+    var visualSetting = visualSelect.value;
+    console.log(visualSetting);
+
+      analyser.fftSize = 256;
+      var bufferLengthAlt = analyser.frequencyBinCount;
+      console.log(bufferLengthAlt);
+      var dataArrayAlt = new Uint8Array(bufferLengthAlt);
+
+      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+      var drawAlt = function() {
+        drawVisual = requestAnimationFrame(drawAlt);
+
+        analyser.getByteFrequencyData(dataArrayAlt);
+
+        canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+        var barWidth = (WIDTH / bufferLengthAlt) * 2.5;
+        var barHeight;
+        var x = 0;
+
+        for(var i = 0; i < bufferLengthAlt; i++) {
+          barHeight = dataArrayAlt[i];
+
+          canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+          canvasCtx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
+
+          x += barWidth + 1;
+        }
+      };
+
+      drawAlt();
+    }
+  }
+
+
+function addTrack()
+{
+  //alert("This button will add a new melody line to the score");
+  console.log("addTrack was clicked");
 }
 
 function record()
 {
-  background(0);
-  micLevel = mic.getLevel();
-  ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
-}
-
-function addTrack()
-{
-  alert("This button will add a new melody line to the score");
+  //alert("This button will add a new melody line to the score");
+  console.log("record was clicked");
 }
 
 function stop()
 {
-  alert("This button will stop recording audio");
+  //alert("This button will stop recording audio");
+  console.log("stop was clicked");
 }
 
 function play()
 {
-  alert("This button will play the recorded melody");
+  //alert("This button will play the recorded melody");
+  console.log("play was clicked");
 }
 
 function pause()
 {
-  alert("This button will pause the recorded melody");
+  //alert("This button will pause the recorded melody");
+  console.log("pause was clicked");
 }
-
-setup();
